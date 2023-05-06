263bf0ab03178ae063e50de0a3ef2c1e061fe2aa
duyn <khanhduy.dev@gmail.com>
2023-05-06 10:38:32 +0700

    intital commit

---
+++ src/main/java/com/ms/moneycontroller/MoneyController.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ms.moneycontroller;
+
+/**
+ *
+ * @author khanh
+ */
+public class MoneyController {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
